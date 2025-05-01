77d786a7529e23076131b953a4a5435a933fb603
Rell10101 <anton.samaev321@mail.ru>
2025-05-01 16:36:03 +0900

    route doc

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-//use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\ChatController;
@@ -9,9 +8,10 @@ use App\Http\Controllers\ManagerController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
 
-//Route::get('/', 'MainController@home');
+// маршрут на главную страницу
 Route::get('/', [MainController::class, 'home']);
 
+// маршруты для регистрации, входа и выхода из аккаунта
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
@@ -20,41 +20,43 @@ Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard'); // Создайте представление dashboard.blade.php
+        return view('dashboard'); 
     })->name('dashboard');
 });
 
+// маршрут на профиль
 Route::get('/profile', [MainController::class, 'profile_show'])->middleware('auth');
 
 
-//Route::get('/about', 'MainController@about');
-//Route::get('/about', [MainController::class, "about"] );
+// отправка заявки
 Route::get('/request', [MainController::class, "request"] );
 Route::post('/request/send', [MainController::class, "request_send"] );
-// Route::get('/request_show', [MainController::class, "request_show"] );
 
+// действия исполнителя
 Route::post('/requests/{id}/accept', [MainController::class, 'accept'])->name('requests.accept');
 Route::post('/requests/{id}/complete', [MainController::class, 'complete'])->name('requests.complete');
 Route::post('/requests/{id}/decline', [MainController::class, 'decline'])->name('requests.decline');
 Route::post('/requests/{id}/not-completed', [MainController::class, 'markAsNotCompleted'])->name('requests.markAsNotCompleted');
 
+// удаления заявки
 Route::delete('/requests/{id}', [MainController::class, 'destroy'])->name('requests.destroy');
 
+// действия менеджера
 Route::post('/requests/{id}/check', [ManagerController::class, 'check'])->name('requests.check');
 Route::post('/requests/{id}/mark-checked', [ManagerController::class, 'markAsChecked'])->name('requests.markChecked');
 
-
+// действия администратора
 Route::get('/users_show', [AdminController::class, 'users_show'])->name('users.users_show');
 Route::post('/users/{id}/role_id', [AdminController::class, 'updateRole'])->name('users.updateRole');
 
+// показ заявок
 Route::get('/request_show', [MainController::class, 'request_show'])->name('requests.request_show');
 
 Route::get('/review', [MainController::class, "review"] )->name('review');
 Route::post('/review/check', [MainController::class, "review_check"] );
 
+// чат-бот
 Route::get('/chat', [ChatController::class, 'chat'])->name('chat');
 Route::post('/chat_accept', [ChatController::class, "chat_accept"] );
 
-// Route::get('/user/{id}/{name}', function ($id, $name) {
-//     return 'ID: '. $id.'. Name: '.$name;
-// });
+
